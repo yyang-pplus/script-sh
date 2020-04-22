a53620e8fbe65a647446678d0e2cd14e474557c1
Yang Yang <yyang.even@gmail.com>
2020-04-22 12:58:03 -0700

    Bash: Add duplicate_lines.sh
    Change-Id: I25cdcd19a725e2bff3d2ec2627aaf92e59a28a91

---
+++ duplicate_lines.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+##
+# @reference    Find duplicate lines in a file and count how many time each line was duplicated?
+#               https://stackoverflow.com/questions/6712437/find-duplicate-lines-in-a-file-and-count-how-many-time-each-line-was-duplicated
+##
+if [ $# -eq 1 ]; then
+    sort "$1" | uniq --count --repeated
+else
+    echo "Usage: $(basename $0) <file>"
+    exit 1
+fi
--- run_until_fail.sh
+++ run_until_fail.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-##
-# Usage: run_until_fail.sh [command]
-# Example: run_until_fail.sh ctest --output-on-failure -R "test_name_pattern"
-##
 
-while "$@"; do :; done
+##
+# @reference    Repeatedly run a shell command until it fails?
+#               https://stackoverflow.com/questions/12967232/repeatedly-run-a-shell-command-until-it-fails
+##
+if [ $# -eq 0 ]; then
+    echo "Usage: $(basename $0) <command>"
+    echo " Example: run_until_fail.sh ctest --output-on-failure -R <regex>"
+    exit 1
+else
+    while "$@"; do :; done
+fi
