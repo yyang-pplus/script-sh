055c8fe3c9742806e012f1c93d5817a62627137d
Yang Yang <yyang.pplus@gmail.com>
2020-04-24 15:19:59 -0700

    Scripts: Replace $N with more readable variable names
    Change-Id: Iad5121b1a509e004bd3a31ab1adc491b4c9b771a

--- duplicate_lines.sh
+++ duplicate_lines.sh
@@ -6,7 +6,8 @@
 #               https://stackoverflow.com/questions/6712437/find-duplicate-lines-in-a-file-and-count-how-many-time-each-line-was-duplicated
 ##
 if [ $# -eq 1 ]; then
-    sort "$1" | uniq --count --repeated
+    FILE="$1"
+    sort "$FILE" | uniq --count --repeated
 else
     echo "Usage: $(basename $0) <file>"
     exit 1
