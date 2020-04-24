5be405ec0f141d035a3240c729dd57a9444ade21
Yang Yang <yyang.even@gmail.com>
2020-04-24 16:46:16 -0700

    Git: Hooks: Add pre-push
    Change-Id: Iebd5be52e1d75e71f9cc4f3f38ea0be046faefcf

--- git/safe_push.sh
+++ git/safe_push.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-source ~/.bash_util.sh
-
 ##
 # Note: The script requires:
 #   git config --global push.default current
 ##
+
+
+source ~/.bash_util.sh
+
+THIS_DIR=$(dirname "$0")
+source $THIS_DIR/util.sh
+
 
 ##
 # @reference    How can I see which Git branches are tracking which remote / upstream branch?
@@ -13,17 +18,6 @@ source ~/.bash_util.sh
 ##
 CURRENT_UPSTREAM=$(git rev-parse --abbrev-ref --symbolic-full-name @{upstream})
 
-PATTERN="(master)|$GIT_PROTECTED_BRANCH_PATTERN"
-
-##
-# @reference    Bash test: what does “=~” do?
-#               https://unix.stackexchange.com/questions/340440/bash-test-what-does-do
-#
-# @note The string, to be matched, should be quoted.
-#       The regular expression pattern shouldn't be quoted.
-##
-if [[ "$CURRENT_UPSTREAM" =~ $PATTERN ]]; then
-    Fatal "Push to $CURRENT_UPSTREAM is restricted."
-else
+if ! IsProtected "$CURRENT_UPSTREAM"; then
     git push "$@"
 fi
