b39af7916af3b630cf433da70af026de05b25aa0
Yang Yang <yyang.even@gmail.com>
2020-04-24 12:45:01 -0700

    Git: Add safe_push.sh
    Change-Id: I47a9e80342947adc63d82364a31e3e7aedff8ff7

---
+++ git/safe_push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ~/.bash_util.sh
+
+##
+# Note: The script requires:
+#   git config --global push.default current
+##
+
+##
+# @reference    How can I see which Git branches are tracking which remote / upstream branch?
+#               https://stackoverflow.com/questions/4950725/how-can-i-see-which-git-branches-are-tracking-which-remote-upstream-branch
+##
+CURRENT_UPSTREAM=$(git rev-parse --abbrev-ref --symbolic-full-name @{upstream})
+
+##
+# @reference    Bash test: what does “=~” do?
+#               https://unix.stackexchange.com/questions/340440/bash-test-what-does-do
+#
+# @note The string, to be matched, should be quoted.
+#       The regular expression pattern shouldn't be quoted.
+##
+PATTERN="(master)|$GIT_PROTECTED_BRANCH_PATTERN"
+
+if [[ "$CURRENT_UPSTREAM" =~ $PATTERN ]]; then
+    Fatal "Push to $CURRENT_UPSTREAM is restricted."
+else
+    :
+    #git push "$@"
+fi
