ea4b70ad2c867234ac24d8957406f25c466e8d6c
Yang Yang <yyang.pplus@gmail.com>
2020-04-30 16:58:44 -0700

    Tests: Replace Assert() with shUnit2
    Change-Id: I2fc74765b7871e3fae90c4b92dd20bcbaef55b3a

--- git/replace_all.sh
+++ git/replace_all.sh
@@ -7,17 +7,14 @@ source ~/.bash_util.sh
 # @reference    Use Git Grep to Replace Strings in Files in Your Git Repository
 #               https://blog.jasonmeridth.com/posts/use-git-grep-to-replace-strings-in-files-in-your-git-repository/
 ##
-if [ $# -eq 1 ]; then
-    DIRECTORY=$(pwd)
-elif [ $# -eq 2 ]; then
-    DIRECTORY="$2"
-else
+if [ $# -eq 0 ]; then
     echo "Usage: $(basename $0) <sed_command> [<directory>]"
     echo " Example: git_sed.sh 's/status/balabala/g'"
     echo " Example: git_sed.sh 's/status/balabala/g scripts'"
     exit 1
 fi
 
+DIRECTORY=${2:-$(pwd)}
 SED_COMMAND="$1"
 
 SEARCH_KEY=`echo "$SED_COMMAND" | sed -n -E 's%^.*/(.*)/(.*)/.*$%\1%p'`
