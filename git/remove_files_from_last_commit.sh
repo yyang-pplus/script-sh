43b1cf3b156fce33410df4021e8895c3dbcdea0c
Yang Yang <yyang.pplus@gmail.com>
2020-05-05 17:32:54 -0700

    Git: Add remove_files_from_last_commit.sh
    Change-Id: Ic7b3b7ed53a8820288a64bea86d970229a9eae1a

---
+++ git/remove_files_from_last_commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $(basename $0) [--] <paths>..."
+    exit 1
+fi
+
+##
+# @reference    Remove files from Git commit
+#               https://stackoverflow.com/questions/12481639/remove-files-from-git-commit
+##
+git reset --soft HEAD^
+git reset HEAD "$@"
+git commit --reuse-message ORIG_HEAD
