5d07eaa56f2bf430b7db6001b3b4c9b6c4673b4c
Yang Yang <yyang.pplus@gmail.com>
2020-08-27 10:35:35 -0700

    Git: Improve astyle_and_build.sh
    Change-Id: I43b24f11e64b311c2638c5cc8adbed04822786c2

--- git/astyle_and_build.sh
+++ git/astyle_and_build.sh
@@ -3,11 +3,14 @@
 source ~/.bash_util.sh
 
 
+set -ex
+
+
 NUMBER_CORES=$(grep ^cpu\\scores /proc/cpuinfo | awk '{print $4}' | uniq)
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 
 QuietRun pushd "$PROJECT_ROOT_DIR"
-    astyle --recursive --options="scripts/astyle.rc" "*.cpp" "*.h"
+    astyle "*.c, *.cpp, *.h, *.hpp, *.cs"
 
     pushd build
         echo "Compiling with jobs:" $NUMBER_CORES
