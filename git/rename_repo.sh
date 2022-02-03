ab8762d61b0a7280a36ce83d7c57a3e5d850408f
Yang Yang <yyang.pplus@gmail.com>
2022-02-02 22:58:41 -0800

    Git: Improve rename_repo to fix travis CI badge

--- git/rename_repo.sh
+++ git/rename_repo.sh
@@ -15,10 +15,18 @@ OLD_NAME="psqlxx"
 TEMPLATE_NAME="cpp-template-git"
 OLD_SUMMARY="<Project Description Summary>"
 
-git mv "$REPO_DIR/$OLD_NAME" "$REPO_DIR/$new_name"
+pushd $REPO_DIR
+git mv "$OLD_NAME" "$new_name"
 
-sed -i "/$OLD_SUMMARY/q" "$REPO_DIR/README.md"
+sed -i "/$OLD_SUMMARY/q" "README.md"
+
+EMAIL=$(git config user.email)
+NICK_NAME="even"
+if [[ "$EMAIL" == "*$NICK_NAME@*" ]]; then
+    sed -i "s/-pplus/-$NICK_NAME/g" "README.md"
+fi
 
 $THIS_DIR/replace_all.sh "s/$OLD_NAME/$new_name/g" "$REPO_DIR"
 $THIS_DIR/replace_all.sh "s/$TEMPLATE_NAME/$new_name/g" "$REPO_DIR"
 $THIS_DIR/replace_all.sh "s/$OLD_SUMMARY/$summary/g" "$REPO_DIR"
+popd
