d3bd162eaaeea0752fe3f982d8be742d72aa3be0
Yang Yang <yyang.pplus@gmail.com>
2022-05-04 22:27:36 -0700

    Pre-commit: Replace astyle with clang-format

--- git/build.sh
+++ git/build.sh
@@ -7,11 +7,7 @@ set -e
 NUMBER_CORES=$(grep ^cpu\\scores /proc/cpuinfo | awk '{print $4}' | uniq)
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 
-QuietRun pushd "$PROJECT_ROOT_DIR"
-astyle --recursive "*.c, *.cpp, *.h, *.hpp, *.cs"
-
-pushd build
+QuietRun pushd "$PROJECT_ROOT_DIR/build"
 echo "Compiling with jobs:" $NUMBER_CORES
 make --jobs $NUMBER_CORES
-popd
 QuietRun popd
