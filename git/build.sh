b068b22fa7765fb02d4d5c98eea66ff18007acbe
Yang Yang <yyang.pplus@gmail.com>
2022-06-01 22:36:12 -0700

    Git: Improve build script for auto-discovery cmake

--- git/build.sh
+++ git/build.sh
@@ -7,6 +7,9 @@ set -e
 NUMBER_CORES=$(grep ^cpu\\scores /proc/cpuinfo | awk '{print $4}' | uniq)
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 
+# For auto-discovery
+touch "$PROJECT_ROOT_DIR/CMakeLists.txt"
+
 QuietRun pushd "$PROJECT_ROOT_DIR/build"
 echo "Compiling with jobs:" $NUMBER_CORES
 make --jobs $NUMBER_CORES
