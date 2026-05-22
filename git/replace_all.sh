f979a02d88a4168ebeaef87b1c895a7471b2a3a7
Yang Yang <yyang.pplus@gmail.com>
2026-05-22 11:02:59 -0700

    Git: Improve replace_all.sh

--- git/replace_all.sh
+++ git/replace_all.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-source ~/.bash_util.sh
-
-set -x
-
 ##
 # @reference    Use Git Grep to Replace Strings in Files in Your Git Repository
 #               https://blog.jasonmeridth.com/posts/use-git-grep-to-replace-strings-in-files-in-your-git-repository/
 ##
 if [ $# -eq 0 ]; then
     echo "Usage: $(basename $0) <sed_command> [<directory>]"
-    echo " Example: git_sed.sh 's/status/balabala/g'"
-    echo " Example: git_sed.sh 's/status/balabala/g scripts'"
+    echo " Example: $(basename $0) 's/status/balabala/g'"
+    echo " Example: $(basename $0) 's/status/balabala/g' scripts"
     exit 1
 fi
 
@@ -20,6 +16,6 @@ SED_COMMAND="$1"
 
 SEARCH_KEY=$(echo "$SED_COMMAND" | sed -n -E 's%^.*/(.*)/(.*)/.*$%\1%p')
 
-QuietRun pushd "$DIRECTORY"
-git grep --files-with-matches --null -E "$SEARCH_KEY" . | xargs -0 sed -i -E "$SED_COMMAND"
-QuietRun popd
+set -x
+
+git grep --files-with-matches --null -E "$SEARCH_KEY" "$DIRECTORY" | xargs -0 sed -i -E "$SED_COMMAND"
